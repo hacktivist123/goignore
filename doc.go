6bc7812da6960cc6314e0fc9c8fc2f96760884d8
Shedrack akintayo <akintayoshedrack@gmail.com>
2023-08-04 02:26:56 +0100

    add doc.go

---
+++ doc.go
@@ -0,0 +1,11 @@
+// Package goignore is a lightweight CLI tool for generating .gitignore files based on programming languages.
+// It provides functionality to create .gitignore files for specific languages and supports auto-detection.
+//
+// Usage:
+//
+//	goignore new -l=[language]   Generate a .gitignore file for the specified programming language
+//	goignore list                List all supported programming languages
+//	goigbore new -l=[auto]       Automatically detect files in the project repo and generate .gitignore
+//
+// For more information, check out the GitHub repository: https://github.com/hacktivist123/goignore
+package goignore
